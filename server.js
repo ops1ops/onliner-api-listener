a3906e158401d6263cdf24c42bfdb71d8719bdd6
ops1ops <gidegode1940@gmail.com>
2020-01-18 14:18:16 +0300

    eslint added, fixed bug in listener

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
-const users  = require('./routes/users');
+const users = require('./routes/users');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.set("json spaces", 4);
+app.set('json spaces', 4);
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
   res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'x-access-token, Origin, Content-Type, Accept',
   );
   next();
 });
