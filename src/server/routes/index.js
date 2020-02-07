bc30ae6dfa2e0a645c7230d6ec663ad6f4160834
dexter0175 <dexter0175@gmail.com>
2020-02-07 23:14:41 +0300

    Update index.js

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,9 +7,9 @@ const { Item } = db;
 const router = Router();
 
 router.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-router.get('/api', async (req, res) => {
+router.get('/api', async ({ query: { query } }, res) => {
   let items;
-  if (req.query.query) {
+  if (query) {
     items = await Item.findAll({
       attributes: ['name', 'htmlUrl', 'imageUrl', 'price'],
       include: ['history'],
