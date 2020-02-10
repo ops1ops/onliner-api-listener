42de49bd1d6a8c4372e686cf0d1da0104d6a0b4d
Ilya <49243022+ops1ops@users.noreply.github.com>
2020-02-10 13:29:02 +0300

    fix server error with dev, minor refactoring (#20)
    * fix server error with dev, minor refactoring
    
    * add dotenv for prod mode
    
    * refactoring server script

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,8 +7,10 @@ const { Item } = db;
 const router = Router();
 
 router.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
 router.get('/api', async ({ query: { query } }, res) => {
   let items;
+
   if (query) {
     items = await Item.findAll({
       attributes: ['name', 'htmlUrl', 'imageUrl', 'price'],
