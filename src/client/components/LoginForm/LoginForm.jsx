d0c91084560dbd06324048c0cc1b31b58825b015
dexter0175 <dexter0175@gmail.com>
2020-02-11 09:32:34 -0800

    added LoginForm (#22)
    * added LoginForm
    
    added LoginForm
    
    * refactoring

---
+++ src/client/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useCallback } from 'react';
+import { Paper, Button, FormControl, OutlinedInput, InputLabel, Typography } from '@material-ui/core';
+import './LoginForm.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+function LoginForm() {
+  const classes = useStyles();
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLoginChange = useCallback(({ target: { value } }) => setLogin(value), []);
+  const handlePasswordChange = useCallback(({ target: { value } }) => setPassword(value), []);
+
+  return (
+    <Paper elevation={3} className="form-box">
+      <form className={classes.root}>
+        <Typography gutterBottom variant="h5" component="h2">
+          Login
+        </Typography>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="component-outlined">Login</InputLabel>
+          <OutlinedInput
+            id="input-login"
+            value={login}
+            label="Login"
+            onChange={handleLoginChange}
+          />
+        </FormControl>
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="component-outlined">Password</InputLabel>
+          <OutlinedInput
+            id="input-pass"
+            value={password}
+            label="Passwword"
+            type="password"
+            onChange={handlePasswordChange}
+          />
+        </FormControl>
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
+      </form>
+    </Paper>
+  );
+}
+
+export default LoginForm;
