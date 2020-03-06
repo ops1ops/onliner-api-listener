3c2af57eefd92306cbcdb4b1e8093f0024bfb90c
Siarhei Bahamiaka <dexter0175@gmail.com>
2020-03-06 03:20:31 +0300

    get subcriptions

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { createUser, getUserById, loginUser } from '../controllers/users';
+import { getUserItems } from '../controllers/items';
 import auth from '../middlewares/auth';
 
 const router = Router();
 
 router.post('/users/create', createUser);
 router.post('/users/login', loginUser);
+router.get('/user/subscriptions', auth, getUserItems);
 router.get('/user/:id', auth, getUserById);
 
 export default router;
