cc97cb831779623333abc98cecc42303223488b0
Ilya Drozdovich <i.drozdovich@itransition.com>
2020-03-06 12:38:25 +0300

    added pagination for categories

--- src/client/Routes.jsx
+++ src/client/Routes.jsx
@@ -4,14 +4,14 @@ import { Route, Switch } from 'react-router';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import ItemPage from './components/ItemPage/ItemPage';
-import UserPage from './components/UserPage';
+import HomePage from './components/HomePage';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/login" component={LoginPage} />
     <Route exact path="/register" component={RegisterPage} />
-    <Route exact path="/user" component={UserPage} />
-    <Route exact path="/" component={UserPage} />
+    <Route exact path="/user" component={HomePage} />
+    <Route exact path="/" component={HomePage} />
     <Route exact path="/item/:key" component={ItemPage} />
   </Switch>
 );
--- src/client/services/api.js
+++ src/client/services/api.js
@@ -24,8 +24,8 @@ export const getItemByKey = (id) => axios.get(`/api/item/${id}`);
 export const getCategories = () => axios
   .get('/api/categories');
 
-export const getCategory = (categoryKey) => axios
-  .get(`/api/categories/${categoryKey}`, { headers: getAuthHeaders() });
+export const getCategory = (categoryKey, pageNumber = 1) => axios
+  .get(`/api/categories/${categoryKey}?page=${pageNumber}`, { headers: getAuthHeaders() });
 
 export const searchItems = (params) => axios
   .get('/api/search/items', { params: { query: params }, headers: getAuthHeaders() });
