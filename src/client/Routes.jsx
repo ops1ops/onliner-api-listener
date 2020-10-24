5414aa068f53c62b6ca7cb15f5e2f21218238c40
Ilya <49243022+ops1ops@users.noreply.github.com>
2020-10-24 19:23:35 +0300

    changed table pagination, added creation date (#57)

--- src/client/Routes.jsx
+++ src/client/Routes.jsx
@@ -5,7 +5,7 @@ import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import ItemPage from './components/ItemPage/ItemPage';
 import HomePage from './components/HomePage';
-import UserItemPage from './components/UserItemsPage';
+import UserItemsPage from './components/UserItemsPage';
 
 const Routes = () => (
   <Switch>
@@ -14,7 +14,7 @@ const Routes = () => (
     <Route exact path="/user" component={HomePage} />
     <Route exact path="/" component={HomePage} />
     <Route exact path="/item/:key" component={ItemPage} />
-    <Route exact path="/user/items" component={UserItemPage} />
+    <Route exact path="/user/items" component={UserItemsPage} />
   </Switch>
 );
 
---
+++ src/client/components/UserItemsPage/pageSizes.js
@@ -0,0 +1,5 @@
+export const SMALL_SIZE = 20;
+const MEDIUM_SIZE = 50;
+const LARGE_SIZE = 100;
+
+export const PAGE_SIZES = [SMALL_SIZE, MEDIUM_SIZE, LARGE_SIZE];
